d4a2429f7ed3c904960a9d5693dccea948757281
Valentin Bonnet <1476834+vbonnet@users.noreply.github.com>
2026-07-27 00:36:26 -0700

    test(agm): register BDD definitions once

--- agm/test/bdd/steps/doc.go
+++ agm/test/bdd/steps/doc.go
@@ -1,5 +1,5 @@
 // Package steps holds the godog step definitions that make AGM's SPEC
 // invariants executable. Each feature file under test/bdd/features/ has a
 // matching <name>_steps.go that registers its steps via a Register*Steps
-// function wired into InitializeScenario in main_test.go.
+// function wired into RegisterScenarioDefinitions in main_test.go.
 package steps
